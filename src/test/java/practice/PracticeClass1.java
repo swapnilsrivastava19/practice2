21119bb64410ae32797ac545d55ae414a0ccc64d
swapnil <swapnilsrivastava92.ss@gmail.com>
2023-03-10 15:45:53 +0530

    first commit

---
+++ src/test/java/practice/PracticeClass1.java
@@ -0,0 +1,18 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class PracticeClass1 {
+
+	@Test
+	public void main1() {
+		
+		System.out.println("Test1 --> abc");
+	}
+	
+	@Test
+	public void main11() {
+		System.out.println("Test1 --> xyz");
+	}
+	
+}
---
+++ src/test/java/practice/PracticeClass2.java
@@ -0,0 +1,18 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class PracticeClass2 {
+	
+	@Test
+	public void main2() {
+		System.out.println("Test 2 --> abc");
+	}
+	
+	@Test
+	public void main21() {
+		System.out.println("Test 2 --> xyz");
+	}
+
+
+}
